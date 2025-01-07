4212d0a3e57733b967db0e3e671ca633be34b3cc
Lorant7 <tomy.lorant@gmail.com>
2025-01-06 19:41:29 -0500

    Added skills and contact

---
+++ js/script.js
@@ -0,0 +1,8 @@
+window.onscroll = function () {
+    var navbar = document.querySelector('.navbar');
+    if (window.scrollY > 100) { // Change 100 to whatever scroll distance you want
+        navbar.classList.add('faded');
+    } else {
+        navbar.classList.remove('faded');
+    }
+};
