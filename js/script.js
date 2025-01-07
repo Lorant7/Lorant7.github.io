94c490bec78a42e90fbacb5036f3f2f0038f6605
Lorant7 <tomy.lorant@gmail.com>
2025-01-07 10:28:19 -0500

    added other pages, need to prettify and do blog page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 window.onscroll = function () {
     var navbar = document.querySelector('.navbar');
-    if (window.scrollY > 100) { // Change 100 to whatever scroll distance you want
+    if (window.scrollY > 80) { // Change 100 to whatever scroll distance you want
         navbar.classList.add('faded');
     } else {
         navbar.classList.remove('faded');
